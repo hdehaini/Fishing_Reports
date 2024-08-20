a6202f7edc58ace7157c686b223495e3c8ec4106
hsdehaini <99140276+hsdehaini@users.noreply.github.com>
2024-08-19 20:06:32 -0700

    Refresh Reports

--- js-scripts/chart.js
+++ js-scripts/chart.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("../database/daily_averages.csv")
+  fetch("database/daily_averages.csv")
     .then((response) => {
       if (!response.ok) {
         throw new Error("Network response was not ok");
